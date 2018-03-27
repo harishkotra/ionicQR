5016749c834bbda1b0fc5bba15826e0529b98895
Harish Kotra <harish@deepredink.com>
2018-03-27 09:37:09 +0530

    Working demo of ionicQR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//add our scanner modules
+import {NgxQRCodeModule} from 'ngx-qrcode2';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgxQRCodeModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +29,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//import barcode scanner module
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  //declare variables
+  qrData = null;
+  createdCode = null;
+  scannedCode = null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
 
+  }
+
+  createCode(){
+  	this.createdCode = this.qrData;
+  }
+
+  scanCode(){
+  	this.barcodeScanner.scan().then(barcodeData => {
+  		this.scannedCode = barcodeData.text;
+  	}, (err) => {
+  		//error handling
+  		console.log(err);
+  	});
   }
 
 }
